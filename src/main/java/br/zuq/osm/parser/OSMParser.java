ffe0d55ec4391b25f095725b492ed5b1b1b6a74d
Cam Cook <ckcook@MissionFocuss-MacBook-Pro-2.local>
2014-09-29 15:52:00 -0400

    First

---
+++ src/main/java/br/zuq/osm/parser/OSMParser.java
@@ -0,0 +1,103 @@
+package br.zuq.osm.parser;
+
+import br.zuq.osm.parser.model.OSM;
+import br.zuq.osm.parser.model.OSMNode;
+import br.zuq.osm.parser.model.Relation;
+import br.zuq.osm.parser.model.Way;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
+import java.util.*;
+
+/**
+ * @author Willy Tiengo
+ */
+public class OSMParser {
+
+    /**
+     *
+     */
+    public static OSM parse(InputStream is) throws Exception {
+
+        Document doc;
+        DocumentBuilder builder;
+
+        Node node;
+        NodeList nodesList;
+
+        Map<String, OSMNode> nodes = new LinkedHashMap<String, OSMNode>();
+
+        builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        doc = builder.parse(is);
+
+        nodesList = doc.getChildNodes().item(0).getChildNodes();
+
+        OSM osm = new OSM();
+        for (int i = 0; i < nodesList.getLength(); i++) {
+
+            node = nodesList.item(i);
+
+            if (NodeParser.isNode(node)) {
+
+                OSMNode osmNode = NodeParser.parseNode(node);
+                nodes.put(osmNode.id, osmNode);
+                osm.getNodes().add(osmNode);
+
+            } else if (WayParser.isWay(node)) {
+                Way way = WayParser.parseWay(node, nodes);
+                osm.getWays().add(way);
+
+            } else if (RelationParser.isRelation(node)) {
+
+                Relation relation = RelationParser.parseRelation(osm, node);
+                osm.getRelations().add(relation);
+
+            }
+        }
+
+        Set<OSMNode> nodeset = new HashSet<>();
+
+        for (String n : nodes.keySet()) {
+            nodeset.add(nodes.get(n));
+        }
+
+        return osm;
+    }
+
+    protected static Map<String, String> parseTags(NodeList nodes) {
+
+        Map<String, String> tags = new HashMap<String, String>();
+
+        for (int i = 0; i < nodes.getLength(); i++) {
+
+            Node node = nodes.item(i);
+
+            if (node.getNodeName().equals("tag")) {
+
+                addTag(tags, node);
+
+            }
+        }
+
+        return tags;
+    }
+
+    private static void addTag(Map<String, String> tags, Node node) {
+        String key = node.getAttributes().getNamedItem("k").getNodeValue();
+        String value = node.getAttributes().getNamedItem("v").getNodeValue();
+
+        if (tags.get(key) != null) {
+
+            tags.put(key, tags.get(key) + ";" + value);
+
+        } else {
+
+            tags.put(key, value);
+
+        }
+    }
+}
---
+++ src/test/java/br/zuq/osm/parser/OSMParserTest.java
@@ -0,0 +1,50 @@
+package br.zuq.osm.parser;
+
+import br.zuq.osm.parser.model.OSM;
+import br.zuq.osm.parser.model.Relation;
+import br.zuq.osm.parser.model.Way;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author zuq
+ */
+public class OSMParserTest {
+
+    public OSMParserTest() {
+    }
+
+    @Test
+    public void testTest() throws Exception {
+        OSM osm = OSMParser.parse(getClass().getResourceAsStream("/montenegro-latest.osm"));
+        assertEquals(990250, osm.getNodes().size());
+        assertEquals(61263, osm.getWays().size());
+        assertEquals(517, osm.getRelations().size());
+
+        boolean found = false;
+        for (Way w : osm.getWays()) {
+            if (w.getName() != null) {
+                if (w.getName().equals("Бучичко гробље")) {
+                    assertTrue(w.getAllTags().containsKey("religion"));
+                    assertTrue(w.getAllTags().containsKey("name"));
+                    assertTrue(w.getAllTags().containsKey("landuse"));
+                    assertTrue(w.getAllTags().containsKey("denomination"));
+                    found = true;
+                }
+            }
+        }
+        assertTrue(found);
+        found = false;
+        for (Relation r : osm.getRelations()) {
+            if (r.getName() != null && r.getName().equals("Čapljina - Hum - Dubrovnik")) {
+                assertTrue(r.getAllTags().containsKey("railway"));
+                assertTrue(r.getAllTags().containsKey("route"));
+                assertTrue(r.getAllTags().containsKey("type"));
+                found = true;
+            }
+        }
+        assertTrue(found);
+    }
+}
